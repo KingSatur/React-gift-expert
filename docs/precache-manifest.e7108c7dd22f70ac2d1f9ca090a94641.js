6612fb36d833a3c568486140df11a9971d65ccf3
Juan David Campillo Cepeda <juancawool@outlook.com>
2020-08-24 22:03:32 -0500

    Cambiando nombre

---
+++ docs/precache-manifest.e7108c7dd22f70ac2d1f9ca090a94641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b597cfb29a70737042487664033718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cca89bac21f96f334b70",
+    "url": "/static/css/main.f2498c8f.chunk.css"
+  },
+  {
+    "revision": "b617679593a3f0bd5eaa",
+    "url": "/static/js/2.eea0565d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eea0565d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca89bac21f96f334b70",
+    "url": "/static/js/main.7f4d5719.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
